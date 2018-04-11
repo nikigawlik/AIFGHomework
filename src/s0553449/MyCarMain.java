382c72942addb28b28fe43ae700fe472b675451a
Nils Gawlik <nilsgawlik@gmx.de>
2018-04-11 15:29:43 +0200

    Make angle approach polynomial

--- src/s0553449/MyCarMain.java
+++ src/s0553449/MyCarMain.java
@@ -3,6 +3,7 @@ package s0553449;
 import java.awt.Point;
 
 import org.lwjgl.LWJGLUtil;
+import org.lwjgl.opengl.GL11;
 
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DriverAction;
@@ -10,20 +11,21 @@ import lenz.htw.ai4g.ai.Info;
 
 public class MyCarMain extends AI {
 
-	private static float BRAKE_ANGLE = 0.3f;
+	private static float BRAKE_ANGLE = 6.14f;
+	private static float MAX_ANGLE_SPD = 200f;
+	private static float APPROACH_POWER = 1.5f;
 	private static float BRAKE_TIME = 1f;
+	
+	private String debugStr = "";
 
 	public MyCarMain(Info info) {
 		super(info);
 	}
 
 	@Override
-	public String getName() {
-		return "Combat Wombat";
-	}
-
-	@Override
 	public DriverAction update(boolean arg0) {
+		debugStr = "";
+		
 		float throttle = info.getMaxAcceleration();
 		float steering = 0f;
 		
@@ -33,7 +35,7 @@ public class MyCarMain extends AI {
 		float deltaX = ncp.x - x;
 		float deltaY = ncp.y - y;
 		
-		float reverse = -1;
+		float reverse = 1;
 		
 		float toAngle = (float) Math.atan2(reverse * deltaY, reverse * deltaX);
 		float fromAngle = info.getOrientation();
@@ -43,15 +45,39 @@ public class MyCarMain extends AI {
 		if (Math.abs(deltaAngle) > 3.1415f)
 			deltaAngle -= 6.283f;
 		
-		if(Math.abs(deltaAngle) < BRAKE_ANGLE) {
-			steering = -info.getAngularVelocity() / BRAKE_TIME;
-		} else {
-			steering = deltaAngle;
-		}
+		// the angular velocity we want to have (linear function)
+		float absDA = Math.abs(deltaAngle);
+		float targetAngleV = (float) Math.pow(absDA/BRAKE_ANGLE, APPROACH_POWER) * MAX_ANGLE_SPD;
+		targetAngleV *= Math.signum(deltaAngle);
+		// clamp
+		targetAngleV = Math.min(Math.max(targetAngleV, -MAX_ANGLE_SPD), MAX_ANGLE_SPD);
 		
-		throttle = reverse * (float) Math.sqrt(deltaX*deltaX + deltaY*deltaY) / 20;
+		steering = targetAngleV - info.getAngularVelocity();
+		
+		if (Math.abs(deltaAngle) < 1.57) {
+			// We are on target (more or less)
+			throttle = info.getMaxAcceleration();			
+		} else {
+			// We are turning, so we drive slower
+			throttle = info.getMaxAcceleration(); // * 0.25f;
+		}
+
+		debugStr += "deltaAngle: " + deltaAngle + "\n";
+		debugStr += "targetAngleV: " + targetAngleV + "\n";
+		debugStr += "current angleV: " + info.getAngularVelocity() + "\n";
 		
 		return new DriverAction(throttle, steering);
+	}
+	
+	@Override
+	public void doDebugStuff() {
+		System.out.println(debugStr);
+	}
+
+
+	@Override
+	public String getName() {
+		return "Combat Wombat";
 	}
 
 	@Override

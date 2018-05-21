ecc479b960e693d18922267c03d717128cee4ea8
Nils Gawlik <nilsgawlik@gmx.de>
2018-05-21 16:46:17 +0200

    Implement template modification

--- src/s0553449/TemplateCar.java
+++ src/s0553449/TemplateCar.java
@@ -1,25 +1,28 @@
 package s0553449;
 
+import lenz.htw.ai4g.ai.DriverAction;
 import lenz.htw.ai4g.ai.Info;
 
 public class /*<n*/TemplateCar/*>*/ extends MyCarMain{
     
-	protected float brakeAngle = /*<f*/6.5f/*>*/;
-	protected float maxAngleSpd = /*<f*/45f/*>*/;
-	protected float approachPower = /*<f*/1.7f/*>*/;
-	protected float turnVelocity = /*<f*/6.1f/*>*/;
-	
-	protected float targetWeight = /*<f*/1f/*>*/;
-	protected float obstacleWeight = /*<f*/10f/*>*/;
-	
-    protected float feelerDistance = /*<f*/20f/*>*/;
-    
     public /*<n*/TemplateCar/*>*/(Info info) {
         super(info);
+        brakeAngle = /*<f*/6.5f/*>*/;
+        maxAngleSpd = /*<f*/45f/*>*/;
+        approachPower = /*<f*/1.7f/*>*/;
+        turnVelocity = /*<f*/6.1f/*>*/;
+        targetWeight = /*<f*/1f/*>*/;
+        obstacleWeight = /*<f*/10f/*>*/;
+        feelerDistance = /*<f*/20f/*>*/;
     }
 
     @Override
     public String getName() {
         return /*<n*/"TemplateCar"/*>*/;
+    }
+
+    @Override
+    public DriverAction update(boolean arg0) {
+        return super.update(arg0);
     }
 }
--- src/s0553449/optimization.py
+++ src/s0553449/optimization.py
@@ -1,4 +1,5 @@
 import re
+import random
 
 print("hello world my dude")
 
@@ -6,13 +7,27 @@ f_orig = open("TemplateCar.java", "r")
 text_orig = f_orig.read()
 f_orig.close()
 
-for i in range(1, 10):
+NUMBER_OF_CARS = 30
+
+for i in range(1, NUMBER_OF_CARS):
     name = "Car" + str(i)
     f_new = open(name + ".java", "w")
 
-    text = text_orig.replace("/*<n*/TemplateCar/*>*/", "/*<n*/" + name + "/*>*/")
-    text = text.replace(' /*<n*/"TemplateCar"/*>*/',
-        '/*<n*/"' + name + '"/*>*/')
+    text = text_orig.replace(
+        "/*<n*/TemplateCar/*>*/",
+        "/*<n*/" + name + "/*>*/"
+    )
+    text = text.replace(
+        '/*<n*/"TemplateCar"/*>*/',
+        '/*<n*/"' + name + '"/*>*/'
+    )
+
+    def repl(match):
+        num = match.group(2)
+        txt = str(float(num) * random.randrange(80, 120) / 100)
+        return match.group(1) + txt + match.group(4)
+
+    text = re.sub(r"(/\*<f\*/)(\d+(\.\d+)?)(f/\*>\*/)", repl, text)
 
     f_new.write(text)
     f_new.close()

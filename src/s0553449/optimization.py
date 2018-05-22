12a54e7be78d5bc913215129cd6cc3102f0291b7
Nils Gawlik <nilsgawlik@gmx.de>
2018-05-22 15:11:59 +0200

    Remove, rename test cars, add to gitignore

--- src/s0553449/optimization.py
+++ src/s0553449/optimization.py
@@ -10,7 +10,7 @@ f_orig.close()
 NUMBER_OF_CARS = 30
 
 for i in range(1, NUMBER_OF_CARS):
-    name = "Car" + str(i)
+    name = "testCar" + str(i)
     f_new = open(name + ".java", "w")
 
     text = text_orig.replace(

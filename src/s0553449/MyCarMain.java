07f50be0c47c4f36f76815e77a2504deb395aa4f
Nils Gawlik <nilsgawlik@gmx.de>
2018-06-05 14:20:21 +0200

    Implement finding graph nodes

--- src/s0553449/MyCarMain.java
+++ src/s0553449/MyCarMain.java
@@ -3,8 +3,10 @@ package s0553449;
 import java.awt.Point;
 import java.awt.Polygon;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector2f;
 
 import lenz.htw.ai4g.ai.AI;
 import lenz.htw.ai4g.ai.DriverAction;
@@ -30,21 +32,52 @@ public class MyCarMain extends AI {
 	private float y; 
 	private float[] debugXs;
 	private float[] debugYs;
+	private Vector2f[] debugPoints = new Vector2f[0];
 
 	public MyCarMain(Info info) {
 		super(info);
 		// enlistForDevelopment();
 		enlistForTournament(553449);
+		calculateGraph();
+	}
+
+	private void calculateGraph() {
+		ArrayList<Vector2f> points = new ArrayList<>();
+		// calculate outer points
+		for (Polygon polygon : info.getTrack().getObstacles()) {
+			int n = polygon.npoints;
+			for(int i = 0; i < n; i++) {
+				Vector2f p1 = new Vector2f(polygon.xpoints[i], polygon.ypoints[i]);
+				Vector2f p2 = new Vector2f(polygon.xpoints[(i+1)%n], polygon.ypoints[(i+1)%n]);
+				Vector2f p3 = new Vector2f(polygon.xpoints[(i+2)%n], polygon.ypoints[(i+2)%n]);
+				
+				Vector2f v1 = new Vector2f();
+				Vector2f.sub(p1, p2, v1);
+				Vector2f v2 = new Vector2f();
+				Vector2f.sub(p3, p1, v2);
+				Vector2f v2o = new Vector2f(-v2.y, v2.x);
+
+				if(Vector2f.dot(v1, v2o) > 0) {
+					points.add(p2);
+				}
+			}
+		}
+
+		debugPoints = points.toArray(debugPoints);
 	}
 
 	@Override
 	public DriverAction update(boolean arg0) {
 		debugStr = "";
 		
+		return doSteering();
+	}
+
+	private DriverAction doSteering() {
 		float throttle = info.getMaxAcceleration();
 		float steering = 0f;
 		
-		Point ncp = info.getCurrentCheckpoint();
+		Point ncp = getCurrentCheckpoint();
 		x = info.getX();
 		y = info.getY();
 		float deltaX = ncp.x - x;
@@ -56,7 +89,7 @@ public class MyCarMain extends AI {
 		deltaX *= targetWeight;
 		deltaY *= targetWeight;
 		
-		debugStr += "pre delta: " + deltaX + ", " + deltaY + "\n";
+		// debugStr += "pre delta: " + deltaX + ", " + deltaY + "\n";
 		
 		Polygon[] obstacles = info.getTrack().getObstacles();
 		
@@ -90,7 +123,7 @@ public class MyCarMain extends AI {
 			debugYs[i+1] = y + dY * feelerDistance;
 		}
 
-		debugStr += "post delta: " + deltaX + ", " + deltaY + "\n";
+		// debugStr += "post delta: " + deltaX + ", " + deltaY + "\n";
 		
 		float reverse = 1;
 		
@@ -122,13 +155,13 @@ public class MyCarMain extends AI {
 
 		throttle *= throttlemod;
 
-		debugStr += "deltaAngle: " + debugFormat.format(deltaAngle) + "\n";
-		debugStr += "targetAngleV: " + debugFormat.format(targetAngleV) + "\n";
-		debugStr += "current angleV: " + debugFormat.format(info.getAngularVelocity()) + "\n";
-		debugStr += "current vel: " + debugFormat.format(info.getVelocity().length()) + "\n";
+		// debugStr += "deltaAngle: " + debugFormat.format(deltaAngle) + "\n";
+		// debugStr += "targetAngleV: " + debugFormat.format(targetAngleV) + "\n";
+		// debugStr += "current angleV: " + debugFormat.format(info.getAngularVelocity()) + "\n";
+		// debugStr += "current vel: " + debugFormat.format(info.getVelocity().length()) + "\n";
 
-		debugStr += "steering chk: " + debugFormat.format(steering/info.getMaxAngularAcceleration()) + "\n";
-		debugStr += "throttle chk: " + debugFormat.format(throttle/info.getMaxAcceleration()) + "\n";
+		// debugStr += "steering chk: " + debugFormat.format(steering/info.getMaxAngularAcceleration()) + "\n";
+		// debugStr += "throttle chk: " + debugFormat.format(throttle/info.getMaxAcceleration()) + "\n";
 		
 		return new DriverAction(throttle, steering);
 	}
@@ -139,7 +172,9 @@ public class MyCarMain extends AI {
 	
 	@Override
 	public void doDebugStuff() {
-		System.out.println(debugStr);
+		if(debugStr.compareTo("") != 0) {
+			System.out.println(debugStr);
+		}
 		
 		if (debugXs.length != 0) {
 			for (int i = 0; i < debugXs.length; i++) {
@@ -150,6 +185,15 @@ public class MyCarMain extends AI {
 				GL11.glVertex2d(debugXs[i], debugYs[i]);
 				GL11.glEnd();
 			}
+		}
+
+		if(debugPoints != null) {
+			GL11.glColor3f(1f, 1f, 1f);
+			GL11.glBegin(GL11.GL_POINTS);
+			for (Vector2f p : debugPoints) {
+				GL11.glVertex2d(p.x, p.y);
+			}
+			GL11.glEnd();
 		}
 	}
 
